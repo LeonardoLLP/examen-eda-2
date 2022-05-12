7e034ebad2c9e0a81151a437baa19022ed9d9522
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-05-12 08:57:59 +0200

    Arreglado pequeño error sintactico y comenzado a analizar el dataset

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import seaborn as sns
 filepath_conversions = "conversiones.csv"
 filepath_navegacion = "navegacion.csv"
 
-conversions = pd.read_csv(filepath_conversions, parse_dates=True)
-navegacion = pd.read_csv(filepath_navegacion, parse_dates=True)
+conversions = pd.read_csv(filepath_conversions, sep=";", parse_dates=True)
+navegacion = pd.read_csv(filepath_navegacion, sep=";", parse_dates=True)
 
 print(conversions.head())
 print(navegacion.head())
+
+print(conversions.columns)
+print(navegacion.columns)
