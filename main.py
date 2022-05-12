b39fffcde7b3cd83da90103440b89d2262b3cced
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-05-12 08:48:05 +0200

    Comenzada lectura de los archivos csv

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    print("Setup completed")
+import pandas as pd
+import seaborn as sns
+
+filepath_conversions = "conversiones.csv"
+filepath_navegacion = "navegacion.csv"
+
+conversions = pd.read_csv(filepath_conversions, parse_dates=True)
+navegacion = pd.read_csv(filepath_navegacion, parse_dates=True)
+
+print(conversions.head())
+print(navegacion.head())
